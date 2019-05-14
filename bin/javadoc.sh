97b35c54f10879fdc3b7735a1c907c57ce65be48
Igor <>
2019-05-14 12:42:30 -0700

    0.10.0 Javadoc

--- bin/javadoc.sh
+++ bin/javadoc.sh
@@ -4,19 +4,18 @@
 # 
 
 
-version=0.9.3
+version=0.10.0
 root_dir=$(cd $(dirname $0)/..; pwd)
 javadoc_dir=${root_dir}/docs
 
 rm -rf ${javadoc_dir}/*
 
-javadoc -d ${javadoc_dir}  \
-   -classpath ${root_dir}/lib/variant-java-client-0.9.3.jar:${root_dir}/lib/variant-core-0.9.3.jar \
+javadoc -Xdoclint:none --allow-script-in-comments -d ${javadoc_dir}  \
+   -classpath ${root_dir}/lib/variant-java-client-${version}.jar:${root_dir}/lib/variant-core-${version}.jar:\
    -sourcepath ${root_dir}/src/main/java \
    -windowtitle "Variant ${version}" \
    -doctitle "Servlet Adapter for Variant Java Client" \
    -header "<a onclick=\"window.top.location.href='http://getvariant.com';\" href=\"#\"> <img style=\"margin-bottom:5px;\" src=\"http://getvariant.com/wp-content/uploads/2016/05/VariantLogoSmall.png\"/> \</a>" \
-   -bottom "Release $version. Updated $(date +"%d %b %Y").<br/> Copyright &copy; 2017 <a onclick=\"window.top.location.href='http://getvariant.com';\" href=\"#\">Variant Inc.</a>" \
+   -bottom "Release $version. Updated $(date +"%d %b %Y").<br/> Copyright &copy; 2019 <a onclick=\"window.top.location.href='http://getvariant.com';\" href=\"#\">Variant Inc.</a>" \
    com.variant.client.servlet         \
-   com.variant.client.servlet.impl    \
    com.variant.client.servlet.util 
